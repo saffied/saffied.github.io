85bdd3d8e00fba0aeeee3a968e4d9bbbbc0716ec
Derek Saffie <saffied@merrimack.edu>
2017-10-18 09:41:17 -0400

    Updated JS for populating text on landing page

--- action.js
+++ action.js
@@ -11,13 +11,19 @@ document.addEventListener('DOMContentLoaded',function(event){
 
 	function typeWriter(i) {
 		
+		var h = document.createElement("h1"); //creating the element
+		h.innerHTML = dataText[i];          //setting its content
+		var typewriter = document.querySelector(".typewriter");
+		typewriter.innerHTML = '';  //remove all children
+		typewriter.appendChild(h);  //add the new h1 element
+		/*
 		var elm = document.querySelector("h1");
         var newElm = elm.cloneNode(true);
         elm.parentNode.replaceChild(newElm, elm);
 		
 		// add next character to h1
 		document.querySelector("h1").innerHTML = dataText[i] +'<span aria-hidden="true"></span>';
-
+		*/
 		// wait for a while and call this function again for next character
 		setTimeout(function() {
 			typeWriter((i + 1)%3)
